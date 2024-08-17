91d27d62fbc578c92c8d536dbf55d9fd53303a87
DaniPoletto <danielapoletto_bep@hotmail.com>
2024-08-17 10:35:44 -0300

    curso concluido

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  e2e: {},
+  e2e: {
+    defaultCommandTimeout: 20000
+  },
   video: false,
 })
--- cypress/e2e/dependent-tests/sample.cy.js
+++ cypress/e2e/dependent-tests/sample.cy.js
@@ -11,28 +11,38 @@ describe('Dependent tests bad practice', () => {
     cy.contains('h1', 'Your Notes').should('be.visible')
   })
 
-  it('creates a note', () => {
+  it('CRUDs a note', () => {
+    //create a note
     cy.contains('Create a new note').click()
 
     cy.get('#content').type('My note')
     cy.contains('Create').click()
 
-    cy.get('.list-group').should('contain', 'My note')
-  })
+    //assert the note was created
+    cy.get('.list-group')
+      .should('contain', 'My note')
+      .click()
 
-  it('edits a note', () => {
-    cy.get('.list-group').contains('My note').click()
+    //update the note
     cy.get('#content').type(' updated')
     cy.contains('Save').click()
 
+    //assert the note was updated
     cy.get('.list-group').should('contain', 'My note updated')
-    cy.get('.list-group:contains(My note updated)').should('be.visible')
-  })
+    cy.get('.list-group:contains(My note updated)')
+      .should('be.visible')
+      .click()
 
-  it('deletes a note', () => {
-    cy.get('.list-group').contains('My note updated').click()
+    //deletes the note
     cy.contains('Delete').click()
 
+    //Asserts the list has at least one item before assertion the note deletion
+    //this way, we make sure the list of notes has already rendered
+    cy.get('.list-group a')
+      .its('length')
+      .should('be.at.least', 1)
+
+    //assert the note was deleted
     cy.get('.list-group:contains(My note updated)').should('not.exist')
   })
 })
